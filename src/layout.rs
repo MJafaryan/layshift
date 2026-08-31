c258561735e1abb4f8b7fbb09b4bc9eae32b0dfb
MJafaryan <m.jafaryan2006@gmail.com>
2026-08-31 12:11:12 +0330

    refactor: improve layout implementation

--- src/layout.rs
+++ src/layout.rs
@@ -17,7 +17,7 @@ pub struct Layout {
 impl Layout {
     pub fn new(layout_name: &str) -> Result<Self, Box<dyn std::error::Error>> {
         let layout_address =
-            Self::parse_layout_name(layout_name).unwrap_or_else(|_| layout_name.to_string());
+            Layout::parse_layout_name(layout_name).unwrap_or_else(|_| layout_name.to_string());
 
         let content = std::fs::read_to_string(layout_address)
             .map_err(|_| format!("Layout {layout_name} not found."))?;
